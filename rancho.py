49d4f3de9333379e13d0793c3c04eea68125a66b
vvnbel <vvnbel@gmail.com>
2024-02-19 22:26:42 +0300

    first commit, init app

---
+++ rancho.py
@@ -0,0 +1,31 @@
+from typing import Optional, Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_task(task: Annotated[STaskAdd, Depends()]):  # async def add_task(task: STaskAdd,):
+    tasks.append(task)
+    return {"ok": True}
+
+
+'''
+@app.get("/tasks")
+def get_tasks():
+    task = Task(name="Запиши это видео")
+    return "Rancho is the base of my sheet"
+'''
